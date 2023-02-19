7b72aa1eb476d878d39ed0dbaed59d25387b7882
malikasemlali <malika.semlali1992@gmail.com>
2023-02-19 13:40:46 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Enter point
+ * Description: "A program prints the alphabet in lowercase excepts"
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; (x <= 'z') && (x != e, q); x++)
+	putchar (x);
+	putchar ('\n');
+	return (0);
+}
