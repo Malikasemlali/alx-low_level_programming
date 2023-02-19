f108f3bd87e41dcbd45457e4635206cb12ab6189
malikasemlali <malika.semlali1992@gmail.com>
2023-02-19 13:45:38 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char x;
 
-	for (x = 'a'; (x <= 'z') && (x != 'e' and 'q'); x++)
+	for (x = 'a'; (x <= 'z') && (x != 'e', 'q'); x++)
 	putchar (x);
 	putchar ('\n');
 	return (0);
