4918ada0693cb107beb15cbd5cd17561b648fd6f
malikasemlali <malika.semlali1992@gmail.com>
2023-02-20 14:56:12 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,9 +10,11 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 		putchar(i);
-	for (i = i + '0'; i <= '9'; i++)
-		putchar(',');
+	if (i < 9)
+	{
+	putchar(',');
 	putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
