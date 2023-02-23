cfc5feb9c3ebb6871e7a57eb40f7bb63b77f04ef
malikasemlali <malika.semlali1992@gmail.com>
2023-02-22 17:34:50 -0800

    7-last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,14 @@ int print_last_digit(int i)
 	int l;
 
 	l = i % 10;
-	if (l > 0)
+	if (l < 0)
 	{
-		_putchar(l);
-		return (l);
+		_putchar(-l + 48);
+		return (-l);
 	}
 	else
 	{
-		_putchar (-l);
-		return (-l);
+		_putchar (l + 48);
+		return (l);
 	}
 }
