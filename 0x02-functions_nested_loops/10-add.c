05309cb3874f5cb1e89130dacecad3da36b50845
malikasemlali <malika.semlali1992@gmail.com>
2023-02-22 18:32:58 -0800

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - check code
+ * @a: numb one
+ * @b: numb two
+ * Description: function that adds two integers
+ * Return: result of the add
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
