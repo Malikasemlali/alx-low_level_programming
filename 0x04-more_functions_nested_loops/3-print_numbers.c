7ba4fe414a9b393d5d2ca9d2f42c62a5d72b92a5
malikasemlali <malika.semlali1992@gmail.com>
2023-02-23 19:42:14 -0800

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - check code
+ * Description: a function that prints the numbers, from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
