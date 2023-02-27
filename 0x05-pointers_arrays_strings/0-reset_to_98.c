fce383dc7f346bbad5f53a2713f74ea691cffeca
malikasemlali <malika.semlali1992@gmail.com>
2023-02-27 13:39:36 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - check code
+ * @n: check pointer
+ * Description: function takes pointer to an int as parameter
+ * Retern: Always 0
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
