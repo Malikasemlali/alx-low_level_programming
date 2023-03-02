42448e6a16d4313076e6ce2c8b6b6415b82501e7
malikasemlali <malika.semlali1992@gmail.com>
2023-03-02 08:55:31 -0800

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - check code
+ * @s1: string one
+ * @s2: string two
+ * Description: function that compares two strings
+ * Return: string to stdout
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+		return (0);
+}
