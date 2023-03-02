a6e3944af16dc004afa0d2a2115fddad7e54fc5c
malikasemlali <malika.semlali1992@gmail.com>
2023-03-02 07:57:11 -0800

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,12 +13,14 @@ char *_strncat(char *dest, char *src, int n)
 	int j;
 
 	while (dest[i] != '\0')
+	{
 		i++;
-	for (j = 0; n > j && src[j] != 0; j++)
+	}
+	for (j = 0; j < n && src[j] != 0; j++)
 	{
 			dest[i] = src[j];
 	i++;
-	dest[i] = '\0';
 	}
+	dest[i] = '\0';
 	return (dest);
 }
